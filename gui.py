4f0b82f332a7b044d45a8ef8576bf07ce43ac6f7
Navneet Gupta <navneet.gupta@reedbusiness.com>
2020-01-21 07:08:29 +0530

    added scrolling

--- gui.py
+++ gui.py
@@ -10,6 +10,10 @@ sentiment_label_list = []
 emoji_label_list = []
 old_record_count = 0
 
+def auxscrollFunction(event):
+	global canvas
+	canvas.configure(scrollregion=canvas.bbox("all"),width=1250,height=650)
+
 def refresh_label():
 	global transcript_label_list
 	global sentiment_label_list
@@ -50,23 +54,36 @@ def add_label(data):
 	emoji_label_list.append(emoji_label_new)
 
 root=tk.Tk()
+root.configure(background='#80c1ff')
 
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-canvas.pack()
+# canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+# canvas.pack()
 	
-background_image = tk.PhotoImage(file='landscape.png')
-root.background_image = background_image
-background_label = tk.Label(root, image=background_image)
-background_label.place(relwidth=1, relheight=1)
+# background_image = tk.PhotoImage(file='landscape.png')
+# root.background_image = background_image
+# background_label = tk.Label(root, image=background_image)
+# background_label.place(relwidth=1, relheight=1)
 
-frame = tk.Frame(root, bg='#80c1ff', bd=5)
-frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+# frame = tk.Frame(root, bg='#80c1ff', bd=5)
+# frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
-button = tk.Button(frame, text="Start Transcription", font=40, command=lambda: refresh_label())
-button.place(relx=0.05, relheight=1, relwidth=0.3)
+# button = tk.Button(frame, text="Start Transcription", font=40, command=lambda: refresh_label())
+# button.place(relx=0.05, relheight=1, relwidth=0.3)
 
-lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
-lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.75, anchor='n')
+root_frame = tk.Frame(root, bg='#80c1ff')
+root_frame.grid(row=0, column=0)
+
+canvas = tk.Canvas(root_frame, bg='#80c1ff')
+
+lower_frame=tk.Frame(canvas, bg='#80c1ff')
+
+scrollb=tk.Scrollbar(root, orient="vertical", command=canvas.yview)
+scrollb.grid(row=0, column=1, sticky='nsew')  #grid scrollbar in master, but
+canvas['yscrollcommand'] = scrollb.set   #attach scrollbar to frameTwo
+
+canvas.create_window((0,0),window=lower_frame,anchor='nw')
+lower_frame.bind("<Configure>", auxscrollFunction)
+canvas.pack(side="left")
 
 header_label_1=tk.Label(lower_frame, text='Transcript', font=60, fg='#E5704B', bg='#80c1ff')
 header_label_1.grid(row=0, column=0)
@@ -88,4 +105,9 @@ emoji_label_list.append(emoji_label)
 lower_frame.grid_columnconfigure(0, weight=2)
 lower_frame.grid_columnconfigure(1, weight=1)
 lower_frame.grid_columnconfigure(2, weight=1)
+
+root.columnconfigure(0, weight=10)
+root.columnconfigure(1, weight=1)
+
+refresh_label()
 root.mainloop()
